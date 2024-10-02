06aeca148af082717bcd21c81336c96e018437af
brunosm16 <brunoskr23@gmail.com>
2024-10-02 00:10:17 -0300

    test: 🧪 http supported methods

---
+++ tests/fixtures/config-test-server.ts
@@ -0,0 +1,31 @@
+import * as createTestServer from 'create-test-server';
+
+export const configTestServer = async () => {
+  const server = await createTestServer();
+
+  server.get('/success', (req, res) => {
+    res.end('Hey this is a successful GET response');
+  });
+
+  server.delete('/success', (req, res) => {
+    res.end('Hey this is a successful DELETE response');
+  });
+
+  server.head('/success', (req, res) => {
+    res.end('');
+  });
+
+  server.patch('/success', (req, res) => {
+    res.end('Hey this is a successful PATCH response');
+  });
+
+  server.post('/success', (req, res) => {
+    res.end('Hey this is a successful POST response');
+  });
+
+  server.put('/success', (req, res) => {
+    res.end('Hey this is a successful PUT response');
+  });
+
+  return server;
+};
---
+++ tests/http-forge.spec.ts
@@ -0,0 +1,71 @@
+import type createTestServer from 'create-test-server';
+
+import httpForge from '@/main';
+
+import { configTestServer } from './fixtures/config-test-server';
+
+type Server = {
+  get: (url: string, response: (() => string) | string) => void;
+} & createTestServer.TestServer;
+
+describe('Http forge tests', () => {
+  let serverTest: Server = null;
+
+  beforeEach(async () => {
+    serverTest = await configTestServer();
+  });
+
+  afterEach(async () => {
+    await serverTest.close();
+  });
+
+  describe('HTTP Supported Methods', () => {
+    it('Should return successfull response for DELETE method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.delete(endpoint, {}).text();
+
+      expect(result).toEqual('Hey this is a successful DELETE response');
+    });
+
+    it('Should return successful response for GET method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.get(endpoint, {}).text();
+
+      expect(result).toEqual('Hey this is a successful GET response');
+    });
+
+    it('Should return successful response for HEAD method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.head(endpoint, {}).text();
+
+      expect(result).toEqual('');
+    });
+
+    it('Should return successful response for PATCH method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.patch(endpoint, {}).text();
+
+      expect(result).toEqual('Hey this is a successful PATCH response');
+    });
+
+    it('Should return successful response for POST method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.post(endpoint, {}).text();
+
+      expect(result).toEqual('Hey this is a successful POST response');
+    });
+
+    it('Should return successful response for PUT method', async () => {
+      const endpoint = `${serverTest.url}/success`;
+
+      const result = await httpForge.put(endpoint, {}).text();
+
+      expect(result).toEqual('Hey this is a successful PUT response');
+    });
+  });
+});
