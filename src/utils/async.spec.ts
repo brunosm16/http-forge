6183e2f2df21c55ebccd7c8d3dc14cf89c1ba873
brunosm16 <brunoskr23@gmail.com>
2024-12-25 14:45:25 -0300

    test: 🧪 fix open handle

--- src/utils/async.spec.ts
+++ src/utils/async.spec.ts
@@ -3,6 +3,10 @@ import { TimeoutError } from '@/errors/timeout-error';
 import { delay, timeout } from './async';
 
 describe('async utilities', () => {
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   describe('delay', () => {
     it('should delay execution for the specified time', async () => {
       const start = Date.now();
@@ -14,8 +18,10 @@ describe('async utilities', () => {
 
   describe('timeout', () => {
     it('should resolve the promise if it completes before the timeout', async () => {
+      jest.useFakeTimers();
       const response = new Response();
       const promise = Promise.resolve(response);
+      jest.advanceTimersByTime(1000);
       const result = await timeout(promise, 1000);
       expect(result).toBe(response);
     });
