df88937d87237dff3ee98f7c7b8ee439e6e34473
brunosm16 <brunoskr23@gmail.com>
2025-01-07 21:48:07 -0300

    test: 🧪 more tests for retry-policy

--- src/core/retry-policy.spec.ts
+++ src/core/retry-policy.spec.ts
@@ -1,3 +1,5 @@
+import type { HttpError } from '@/errors';
+
 import {
   DEFAULT_HTTP_RETRY_ATTEMPTS,
   SUPPORTED_RETRY_AFTER_VERBS,
@@ -5,7 +7,12 @@ import {
   SUPPORTED_RETRY_VERBS,
 } from '@/constants';
 
-import { buildRetryPolicyConfig } from './retry-policy';
+import {
+  buildRetryPolicyConfig,
+  getRetryAfterNumber,
+  getRetryAfterTimeStamp,
+  parseRetryAfter,
+} from './retry-policy';
 
 describe('retry-policy', () => {
   it(`Should use fallback option for 'allowedRetryAfterStatusCodes'`, () => {
@@ -77,5 +84,54 @@ describe('retry-policy', () => {
     });
 
     expect(options.retryLength).toEqual(8);
+  });
+});
+
+describe('retry-policy - utility methods', () => {
+  jest.mock('@/utils', () => ({
+    delay: jest.fn(),
+    isTimeStamp: jest.fn(),
+  }));
+
+  it('Should parse Retry-After header as timestamp', () => {
+    const error = {
+      response: {
+        headers: {
+          get: jest.fn().mockReturnValue('Wed, 21 Oct 2015 07:28:00 GMT'),
+        },
+      },
+    } as unknown as HttpError;
+
+    const retryAfter = parseRetryAfter(error);
+    expect(retryAfter).toBe(
+      getRetryAfterTimeStamp('Wed, 21 Oct 2015 07:28:00 GMT')
+    );
+  });
+
+  it('Should parse Retry-After header as number', () => {
+    const error = {
+      response: {
+        headers: {
+          get: jest.fn().mockReturnValue('120'),
+        },
+      },
+    } as unknown as HttpError;
+
+    const retryAfter = parseRetryAfter(error);
+    expect(retryAfter).toBe(getRetryAfterNumber('120'));
+  });
+
+  it('Should throw error for invalid Retry-After header', () => {
+    const error = {
+      response: {
+        headers: {
+          get: jest.fn().mockReturnValue('invalid'),
+        },
+      },
+    } as unknown as HttpError;
+
+    expect(() => parseRetryAfter(error)).toThrow(
+      `'Retry-After' header must be a number or a timestamp`
+    );
   });
 });
